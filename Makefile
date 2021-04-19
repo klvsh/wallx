44061b243d5b1f44af7ae629e3bb7ae3cb141e98
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2021-04-19 21:01:01 +0530

    basic gradient image.

--- Makefile
+++ Makefile
@@ -3,13 +3,13 @@ init:
 	venv/bin/pip install -r requirements.txt
 
 lint:
-	autopep8 --in-place --aggressive --recursive udemy
-	pylint udemy
+	autopep8 --in-place --aggressive --recursive main.py
+	pylint main.py
 
 clean:
 	find udemy -name "*.pyc" -exec rm -f {} \;
 	find udemy -name "*.pyo" -exec rm -f {} \;
 	find udemy -name "__pycache__" -exec rm -f {} \;
 
-start:
+run:
 	python main.py
---
+++ main.py
@@ -0,0 +1,43 @@
+#
+# References:
+#   - https://note.nkmk.me/en/python-numpy-generate-gradation-image/
+#
+
+import numpy as np
+from PIL import Image
+
+img_width = 1024
+img_height = 728
+
+
+def generate_2d_gradient(start, stop, width, height):
+    # create linspace from start to stop with "width" evenly spaced points
+    # row of pixels, gradient changes from start to stop having
+    # width number of elments
+    row = np.linspace(start, stop, width)
+
+    # create 2 dimensional tile equalling height of the image, with
+    # the same repetition as one row of pixels
+    return np.tile(row, (height, 1))
+
+
+def generate_gradient(width, height, start_rgb, stop_rgb):
+    # create base image data
+    # height, width, 3 per pixel for RGB
+    image_data = np.zeros((height, width, 3), dtype=np.float)
+
+    for i, (start_color, stop_color) in enumerate(zip(start_rgb, stop_rgb)):
+        image_data[:, :, i] = generate_2d_gradient(
+            start_color, stop_color, width, height)
+
+    return image_data
+
+
+def generate_image():
+    img_data = generate_gradient(
+        img_width, img_height, (0, 0, 0), (255, 255, 255))
+    img = Image.fromarray(np.uint8(img_data))
+    img.save('gray_gradient_h.jpg', quality=95)
+
+
+generate_image()

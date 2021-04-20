96e1f9c2950036869c4579466baa720ba471d5c1
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2021-04-20 08:02:08 +0530

    baackdrop.

--- configuration.py
+++ configuration.py
@@ -1,5 +1,11 @@
 import random
 
+class Backdrop:
+    def __init__(self, start_rgba, stop_rgba):
+        self.start_rgba = start_rgba
+        self.stop_rgba = stop_rgba
+
+
 POSSIBLE_PHASES_DAY = "day"
 POSSIBLE_PHASES_NIGHT = "night"
 
@@ -18,11 +24,21 @@ POSSIBLE_STARS = [
     POSSIBLE_STARS_SPARSE,
 ]
 
+POSSIBLE_BACKDROP_DAY_1 = Backdrop((56, 182, 255, 255), (255, 255, 255, 255))
+POSSIBLE_BACKDROP_DAY = [
+    POSSIBLE_BACKDROP_DAY_1,
+]
+
+POSSIBLE_BACKDROP_NIGHT_1 = Backdrop((0, 0, 0, 255), (70, 70, 70, 255))
+POSSIBLE_BACKDROP_NIGHT = [
+    POSSIBLE_BACKDROP_NIGHT_1,
+]
+
 class ImageConfiguration:
-    def __init__(self, phase="day", stars=False):
+    def __init__(self, phase="day", stars=False, backdrop=None):
         self.phase = phase
         self.stars = stars
-
+        self.backdrop = backdrop
 
 class ConfigurationGenerator:
     def __init__(self):
@@ -35,10 +51,18 @@ class ConfigurationGenerator:
         stars = random.choice(POSSIBLE_STARS) \
                     if phase == POSSIBLE_PHASES_NIGHT \
                         else POSSIBLE_STARS_NO_STARS
+        
+        if phase == POSSIBLE_PHASES_NIGHT:
+            backdrop = random.choice(POSSIBLE_BACKDROP_NIGHT)
+        elif phase == POSSIBLE_PHASES_DAY:
+            backdrop = random.choice(POSSIBLE_BACKDROP_DAY)
+        else:
+            backdrop = random.choice(POSSIBLE_BACKDROP_NIGHT)
 
         params = {
             "phase": phase,
             "stars": stars,
+            "backdrop": backdrop,
         }
 
         return ImageConfiguration(**params)
--- main.py
+++ main.py
@@ -27,12 +27,12 @@ def generate_2d_gradient(start, stop, width, height, is_horizontal=False):
     return np.tile(row, (width, 1)).T
 
 
-def generate_gradient(width, height, start_rgb, stop_rgb, is_horizontal=True):
+def generate_gradient(width, height, start_rgba, stop_rgba, is_horizontal=True):
     # create base image data
     # height, width, 3 per pixel for RGB
     image_data = np.zeros((height, width, 4), dtype=np.float)
 
-    for i, (start_color, stop_color) in enumerate(zip(start_rgb, stop_rgb)):
+    for i, (start_color, stop_color) in enumerate(zip(start_rgba, stop_rgba)):
         image_data[:, :, i] = generate_2d_gradient(
             start_color, stop_color, width, height, is_horizontal=is_horizontal)
 
@@ -43,15 +43,13 @@ def generate_image():
     generator = ConfigurationGenerator()
     configuration = generator.generate("arpit_bhayani")
 
-    if configuration.phase == "day":
-        start_rgb = (56, 182, 255, 255)
-        stop_rgb = (255, 255, 255, 255)
-    else:
-        start_rgb = (0, 0, 0, 255)
-        stop_rgb = (70, 70, 70, 255)
-
     img_data = generate_gradient(
-        img_width, img_height, start_rgb, stop_rgb, is_horizontal=False)
+        img_width,
+        img_height,
+        configuration.backdrop.start_rgba,
+        configuration.backdrop.stop_rgba,
+        is_horizontal=False
+    )
 
     # add_stars(img_data, configuration.stars)
 

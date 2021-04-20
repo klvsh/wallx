824321f9d586737ec14b1ce4fb700d10cf3290b0
Arpit Bhayani <arpit.b.bhayani@gmail.com>
2021-04-20 10:35:12 +0530

    terrain as diff function.

--- configuration.py
+++ configuration.py
@@ -34,18 +34,31 @@ POSSIBLE_BACKDROP_NIGHT = [
     POSSIBLE_BACKDROP_NIGHT_1,
 ]
 
+
+POSSIBLE_TERRAINS_DAY_1 = Backdrop((165, 92, 27, 255), (0, 0, 0, 255))
+POSSIBLE_TERRAINS_DAY = [
+    POSSIBLE_TERRAINS_DAY_1,
+]
+
+POSSIBLE_TERRAINS_NIGHT_1 = Backdrop((165, 92, 27, 255), (0, 0, 0, 255))
+POSSIBLE_TERRAINS_NIGHT = [
+    POSSIBLE_TERRAINS_NIGHT_1,
+]
+
 class ImageConfiguration:
-    def __init__(self, phase="day", stars=False, backdrop=None):
+    def __init__(self, phase="day", stars=False, backdrop=None, terrain_shade=None):
         self.phase = phase
         self.stars = stars
         self.backdrop = backdrop
+        self.terrain_shade = terrain_shade
 
 class ConfigurationGenerator:
     def __init__(self):
         pass
 
     def generate(self, seed):
-        print("seed", random.seed(seed))
+        print("seed", seed)
+        random.seed(seed)
         phase = random.choice(POSSIBLE_PHASES)
 
         stars = random.choice(POSSIBLE_STARS) \
@@ -54,15 +67,19 @@ class ConfigurationGenerator:
         
         if phase == POSSIBLE_PHASES_NIGHT:
             backdrop = random.choice(POSSIBLE_BACKDROP_NIGHT)
+            terrain_shade = random.choice(POSSIBLE_TERRAINS_NIGHT)
         elif phase == POSSIBLE_PHASES_DAY:
             backdrop = random.choice(POSSIBLE_BACKDROP_DAY)
+            terrain_shade = random.choice(POSSIBLE_TERRAINS_DAY)
         else:
             backdrop = random.choice(POSSIBLE_BACKDROP_NIGHT)
+            terrain_shade = random.choice(POSSIBLE_TERRAINS_DAY)
 
         params = {
             "phase": phase,
             "stars": stars,
             "backdrop": backdrop,
+            "terrain_shade": terrain_shade,
         }
 
         return ImageConfiguration(**params)
--- main.py
+++ main.py
@@ -27,16 +27,32 @@ def generate_2d_gradient(start, stop, width, height, is_horizontal=False):
     return np.tile(row, (width, 1)).T
 
 
-def generate_gradient(width, height, start_rgba, stop_rgba, is_horizontal=True):
+def generate_gradient(width, height, start_rgba,
+                      stop_rgba, is_horizontal=True):
     # create base image data
     # height, width, 3 per pixel for RGB
-    image_data = np.zeros((height, width, 4), dtype=np.float)
+    image_data = np.zeros((height, width, 4), dtype=float)
 
     for i, (start_color, stop_color) in enumerate(zip(start_rgba, stop_rgba)):
         image_data[:, :, i] = generate_2d_gradient(
             start_color, stop_color, width, height, is_horizontal=is_horizontal)
 
     return image_data
+
+
+def add_terrain(img_data):
+    (height, width, _) = img_data.shape
+    terrain_heights = generate_terrain(width, height)
+
+    # Draw terrain
+    for index, height in enumerate(terrain_heights):
+        height = int(height)
+        line_width = 6
+        for h in range(-line_width // 2, line_width // 2):
+            img_data[height + h, index, 0] = 255
+            img_data[height + h, index, 1] = 255
+            img_data[height + h, index, 2] = 255
+            img_data[height + h, index, 3] = 255 - (abs(h) * 50)
 
 
 def generate_image():
@@ -52,18 +68,7 @@ def generate_image():
     )
 
     # add_stars(img_data, configuration.stars)
-
-    terrain_heights = generate_terrain(img_width, img_height)
-
-    # Draw terrain
-    for index, height in enumerate(terrain_heights):
-        height = int(height)
-        line_width = 6
-        for h in range(-line_width // 2, line_width // 2):
-            img_data[height + h, index, 0] = 255
-            img_data[height + h, index, 1] = 255
-            img_data[height + h, index, 2] = 255
-            img_data[height + h, index, 3] = 255 - (abs(h) * 50)
+    add_terrain(img_data)
 
     img = Image.fromarray(np.uint8(img_data))
     img.save('wallpaper.png')
